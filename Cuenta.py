37125c0eca8c155ed2973284e88fc01df56d695f
Alejandro Portillo <die.portillo01@gmail.com>
2020-10-21 19:10:40 -0600

    actualizando

--- Cuenta.py
+++ Cuenta.py
@@ -1,14 +1,14 @@
-class Finanzas:
+class Cuenta:
     def __init__(self):
         self.cuenta = 0.00
         
     def ingreso(self,ingreso):
         self.cuenta+=ingreso
-        print(f"Has registrado un ingreso de ${ingreso}.")
-        print(f"Tu saldo en cuenta es de ${self.cuenta}.")
+        print(f"\nHas registrado un ingreso de ${ingreso:.2f}.")
+        print(f"\nTu saldo en cuenta es de ${self.cuenta:.2f}.")
         
     def egreso(self,egreso):
         self.cuenta-=egreso
-        print(f"Has retirado ${egreso}.")
-        print(f"Tu saldo en cuenta es de ${self.cuenta:.2f}")
+        print(f"\nHas retirado ${egreso:.2f}.")
+        print(f"\nTu saldo en cuenta es de ${self.cuenta:.2f}")
         
